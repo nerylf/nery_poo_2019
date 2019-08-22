128ed8052e1553cf140908b086d3048fbf62a179
Aluno <asmiurgo@gmail.com>
2019-08-22 11:56:30 -0300

    aula 03_01

--- trab01/Trabalhos.java
+++ trab01/Trabalhos.java
@@ -2,8 +2,10 @@ package trab01;
 
 public class Trabalhos{
     public static void main(String[] args) {
+        
+        /*
         Garrafa garrafa1 = new Garrafa();
-
+        
         garrafa1.cor = "Azul";
         garrafa1.litros = 2;
         garrafa1.mlRestante = 500;
@@ -14,6 +16,16 @@ public class Trabalhos{
         garrafa1.beber(200);
 
         System.out.println(garrafa1.mlRestante);
+        */
+
+        Ventilador arno = new Ventilador();
+
+        arno.showStatus();
+
+        arno.powerOn();
+        arno.showStatus();
+        arno.powerOff();
+        arno.showStatus();
 
     }
 }
